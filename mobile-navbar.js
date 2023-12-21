3f919b46fdf89a4904115d76a4fca364a4fe4e97
Guilherme Rodrigues <guilherme121225@gmail.com>
2023-12-20 22:57:36 -0300

    feat: Accessibility

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -5,6 +5,13 @@ function toggleMenu(event) {
     
     const nav = document.querySelector('.nav');
     nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('area-expanded', active);
+    if(active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');     
+    }
 }
 
 hambuguer.addEventListener('click', toggleMenu);
